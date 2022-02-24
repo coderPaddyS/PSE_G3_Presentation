4eb53d69a33fc1daf9f9048956de952caa73000a
Lukas Zetto <84440453+lukas333333@users.noreply.github.com>
2022-02-25 00:38:50 +0100

    imprive

--- Kapitel/Implementierung/Kapitel/04_Backend/listings/UtilTests.java
+++ Kapitel/Implementierung/Kapitel/04_Backend/listings/UtilTests.java
@@ -1,8 +1,3 @@
-public static void validate(GraphQLTestTemplate graphQLTestTemplate
-    , String testname)
- throws IOException, JSONException {
-    compare(graphQLTestTemplate, testname);
-}
 private static void compare(GraphQLTestTemplate graphQLTestTemplate
     , String testname)
  throws IOException, JSONException {
