a556578fae5473790192f1ec3fbf5cae0e655bf1
Lukas Zetto <84440453+lukas333333@users.noreply.github.com>
2022-02-24 21:59:10 +0100

    test

---
+++ Kapitel/Implementierung/Kapitel/04_Backend/listings/authentifizierung.java
@@ -0,0 +1,11 @@
+private Optional<DecodedJWT> verifyAndDecodeToken(String token) {
+        try {
+            DecodedJWT decodedJWT = JWT.decode(token);
+            JwkProvider provider = new UrlJwkProvider(new URL(
+                    "https://oidc.scc.kit.edu/auth/realms/kit/protocol/openid-connect/certs"));
+            Jwk jwk = provider.get(decodedJWT.getKeyId());
+            JWTVerifier verifier = JWT
+                    .require(Algorithm.RSA256((RSAPublicKey) jwk.getPublicKey(), null))
+                    .build();
+            return Optional.of(verifier.verify(token));
+        } catch(JWTVerificationException | MalformedURLException | JwkException ex) 
